78230d286fa5304834a170b50869e4e30a2c461e
zhangchi02 <1466504436@qq.com>
2020-08-12 17:43:35 +0800

    add servicewithlogback

---
+++ servicewithlogback/service-with-logback/src/main/java/mylearning/servicecomb/log4j2/Log4j2Main.java
@@ -0,0 +1,11 @@
+package mylearning.servicecomb.log4j2;
+
+import org.apache.servicecomb.foundation.common.utils.BeanUtils;
+
+public class Log4j2Main {
+
+  public static void main(String[] args){
+//    Log4jUtils.init();
+    BeanUtils.init();
+  }
+}
